df90a21fd245fed2d245acd7c0c3d369984b4c91
Ela Telequido <etel433@aucklanduni.ac.nz>
2025-10-10 00:54:43 +1300

    Facebook Login Fix

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAlMPJ4sE0X1K3VlQh5Kma9ud3dV_u7oD4",
   authDomain: "travelsense-6d151.firebaseapp.com",
   projectId: "travelsense-6d151",
-  storageBucket: "travelsense-6d151.firebasestorage.app",
+  storageBucket: "travelsense-6d151.appspot.com",
   messagingSenderId: "1053779257703",
   appId: "1:1053779257703:web:d85ce7df8ab2cf2225a7c2",
   measurementId: "G-W8JWYKL58Y"
