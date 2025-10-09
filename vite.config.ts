c9596dc1ae89b0cb7cecf49a9f70b1cd7039491a
Ela Telequido <etel433@aucklanduni.ac.nz>
2025-10-09 21:21:07 +1300

    Fix base for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/',
+    base: '/TravelSense/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
